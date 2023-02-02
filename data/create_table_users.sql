f6c429a6fdd2d6bfb0c334a5476b6952a1c004b0
Emilie Fischer <emil609u@stud.kea.dk>
2023-02-02 16:09:38 +0100

    databases

---
+++ data/create_table_users.sql
@@ -0,0 +1,8 @@
+DROP table users;
+
+CREATE TABLE users(
+    id    TEXT,
+    name  TEXT,
+    last_name TEXT,
+    PRIMARY KEY(id)
+) WITHOUT ROWID;
---
+++ data/insert_into_users.sql
@@ -0,0 +1,2 @@
+INSERT INTO users VALUES("2", "Tomine", "Hansen");
+
