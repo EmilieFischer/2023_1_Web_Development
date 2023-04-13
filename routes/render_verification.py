e5a1fb020d73b8f8f3b6bb3db0e08cba7204191f
Emilie Fischer <emil609u@stud.kea.dk>
2023-04-13 16:01:36 +0200

    videre verification

--- routes/render_verification.py
+++ routes/render_verification.py
@@ -1,7 +1,19 @@
 from bottle import get, template
-
+import x
 
 #########################
 @get("/verification/<user_verification_key>")
-def _():
-    return template("verification")
+def _(user_verification_key):
+    try:
+        db = x.db()
+        rowsInserted=db.execute("UPDATE users SET user_active WHERE user_verification_key=?", (user_verification_key,)).rowcount
+
+        if rowsInserted == 0: raise Exception("user not active")
+        return template("verification", succes=1)
+        return {'info':'Ok'}
+    except Exception as ex:
+        if 'db' in locals(): db.rollback()
+        print(ex)
+        return template("verification", succes=0)
+    finally:
+        if 'db' in locals(): db.close()
